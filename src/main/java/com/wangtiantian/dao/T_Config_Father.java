86701abfeadee7ebf532dc5603adf14291c6b022
asteroidwang <asteroid0318@163.com>
2024-08-19 10:01:22 +0800

    图片代码

--- src/main/java/com/wangtiantian/dao/T_Config_Father.java
+++ src/main/java/com/wangtiantian/dao/T_Config_Father.java
@@ -277,4 +277,8 @@ public class T_Config_Father {
     public ArrayList<Object> get_查找已下载的数据() {
         return method_有条件的查询("select * from " + tableName + " where C_IsFinish =1 ");
     }
+    public ArrayList<Object> method_分页查询已下载的数据10000条每次(int begin) {
+        return method_有条件的查询("SELECT * FROM " + tableName + " where C_IsFinish = 1  ORDER BY C_ID OFFSET " + begin + " ROWS FETCH NEXT 10000 ROWS ONLY");
+    }
+
 }
