53c1c5c48ed07bcc38ffecd81cc2086634f86e4d
asteroidwang <asteroid0318@163.com>
2024-09-12 15:56:02 +0800

    mini

--- src/main/java/com/wangtiantian/runErShouChe/renRenChe/MainRenRenChe.java
+++ src/main/java/com/wangtiantian/runErShouChe/renRenChe/MainRenRenChe.java
@@ -76,6 +76,7 @@ public class MainRenRenChe {
             String cityPinYin = ((RenRenChe_CityData) cityDataList.get(i)).get_C_quanpin();
             String cityId = ((RenRenChe_CityData) cityDataList.get(i)).get_C_CityId();
             String mainUrl = "https://www.renrenche.com/" + cityPinYin + "/ershouche/pn1/?reentries=%7B%22reentry_id%22%3A%22a242c2ba-f37d-48db-bd04-116a27baf712%22%7D";
+            System.out.println(mainUrl);
             try {
                 Thread.sleep(1000 * 60);
             } catch (Exception e) {
