a8cc4320c9775f3970884805310724a5335d18a4
asteroid <asteroid0318@163.com>
2024-09-28 16:37:19 +0800

    air

--- src/main/java/com/wangtiantian/TestTwo.java
+++ src/main/java/com/wangtiantian/TestTwo.java
@@ -6,8 +6,25 @@ public class TestTwo {
 
 
     public static void main(String[] args) {
-      Timer timer = new Timer();
-      timer.schedule(new TestNumber("测试任务"),2000L,1000L*60);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 16); // 控制时
+        calendar.set(Calendar.MINUTE, 30);       // 控制分
+        calendar.set(Calendar.SECOND, 0);       // 控制秒
+
+        Date time = calendar.getTime();         // 得出执行任务的时间,此处为今天的12：00：00
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(
+                new TestNumber("测试任务")
+//                (new TimerTask() {
+//                     public void run() {
+//                         new TestNumber("测试任务");
+//                         System.out.println("-------设定要指定任务--------");
+//                     }
+//                 }
+                , time, 1000 * 60 * 60 * 24);// 这里
+//        Timer timer = new Timer();
+//        timer.schedule(new TestNumber("测试任务"), 2000L, 1000L * 60);
     }
 }
 
